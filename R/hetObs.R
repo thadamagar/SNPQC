6cf5483b1f7037853e47b471b788c105e5317466
Raju Thada Magar <112591052+thadamagar@users.noreply.github.com>
2024-06-17 12:02:11 -0400

    Version0.1

--- R/hetObs.R
+++ R/hetObs.R
@@ -2,7 +2,12 @@ hetObs <-
 function(genoM, threshold = 0.3) {
   hetObs <- colSums(genoM == 1, na.rm = TRUE) / nrow(genoM)
   hetObs_high <- length(which(hetObs > threshold))
+  list_high_hetObs<- data.frame(
+    Sample = names( hetObs [ hetObs > threshold]),
+    HetObs = hetObs[hetObs > threshold], row.names = NULL
+  )
   hetObs_df <- data.frame(Genotype = colnames(genoM), Heterozygosity = hetObs, row.names = NULL)
   return(list(Number_of_sample_with_high_heterozygocity=hetObs_high,
+              Df_high_hetObs = list_high_hetObs,
               Heterozygocity_df=hetObs_df))
 }
